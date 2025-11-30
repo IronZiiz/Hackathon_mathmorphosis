16941e4697bb585a3a371abb59f3ef95df87c7e4
ViniBarce <vinibbordignon@gmail.com>
2025-11-30 20:16:17 -0300

    feat: funções uteis para o dashboard

--- services/AvalicaoDosCursosService.py
+++ services/AvalicaoDosCursosService.py
@@ -22,3 +22,19 @@ class AvaliacaoDosCursosService(DataLoader):
         total = len(df)
         desconhecimento = len(df[df["RESPOSTA"] == "Desconheço"])
         return (desconhecimento / total) * 100
+    def get_concordancia_total(self, path: str) -> int:
+        df = pd.read_csv(path)
+        concordancia = len(df[df["RESPOSTA"] == "Concordo"])
+        return concordancia
+    def get_discordancia_total(self, path: str) -> int:
+        df = pd.read_csv(path)
+        discordancia = len(df[df["RESPOSTA"] == "Discordo"])
+        return discordancia
+    def get_desconhecimento_total(self, path: str) -> int:
+        df = pd.read_csv(path)
+        desconhecimento = len(df[df["RESPOSTA"] == "Desconheço"])
+        return desconhecimento
+    def curso_selecionado(self, path: str, curso_value: str) -> pd.DataFrame:
+        df = pd.read_csv(path)
+        df_curso = df[df["CURSO"] == curso_value]
+        return df_curso
